87749906d404d30585f54f6a6711635aa2693585
Tin Kanjovsky <tkanjovsky@student.unipu.hr>
2022-01-22 19:12:14 +0100

    Dodaje malo sadržaja i komentare

--- dentes_mundus/src/views/Home.vue
+++ dentes_mundus/src/views/Home.vue
@@ -37,17 +37,42 @@
       </div>
     </div>
 
-    <hr class="featurette-divider" />
-    <div class="container" id="onama">
-      <h1>O Nama</h1>
-      <p>skupina zaljubljenika u stomatolgiju</p>
-      <p>pojačajte svoj brand</p>
-      <p>zašto ne</p>
+    <div class="container-fluid" id="onama">
+      <h1 style="text-shadow: 2px 2px 4px #000000">O Nama</h1>
+      <br />
+      <br />
+      <p>
+        Kako bi olakšali zakazivanje termina kod doktora dentalne medicine i
+        smanjili stres za pacijenta došli smo do ideje da cijeli proces
+        olakšamo.
+      </p>
+      <p>Jer zašto ne?</p>
+      <p>
+        Tim bi olakšali stress za pacijenta a uz to i omogucili platformu da se
+        pacijenti i doktori lakše povežu.
+      </p>
+      <p>
+        Svaki pacijent ima mogućnost odabira usluga, ostavljanja komentara i
+        ocjene za pojedinog doktora.<br />
+        Dok svaki doktor može pogledati naručene termine te kontakte naručenih
+        pacijenata.
+      </p>
+      <h3>Alati</h3>
+      <p>Za ovaj projekt koristio sam Vue.js Framework i Firebase.</p>
     </div>
     <div class="b-example-divider"></div>
   </div>
 </template>
 <style lang="scss" scoped>
+#onama {
+  background-color: rgb(96, 198, 238);
+  color: rgb(255, 255, 255);
+
+  // border: solid 1px black;
+  padding: 20px;
+  margin-top: 20px;
+  font-size: 20px;
+}
 .container-uvod h1 {
   color: rgb(39, 111, 139);
   text-shadow: 2px 2px 4px #000000;
